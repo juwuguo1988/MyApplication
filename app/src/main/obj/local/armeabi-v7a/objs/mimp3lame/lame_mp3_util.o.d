633e5a974d6a91c84a53dcb4a57b3ef5fdc76aab
juwuguo1988 <juwuguo1988@126.com>
2020-02-24 16:15:40 +0800

    编译自己项目目录下的so库

---
+++ app/src/main/obj/local/armeabi-v7a/objs/mimp3lame/lame_mp3_util.o.d
@@ -0,0 +1,8 @@
+/Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/obj/local/armeabi-v7a/objs/mimp3lame/lame_mp3_util.o: \
+  /Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/lame_mp3_util.c \
+  /Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/lame_3.99.5_libmp3lame/lame.h \
+  /Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/com_example_hello_myapplication_utils_LameMp3Util.h
+
+/Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/lame_3.99.5_libmp3lame/lame.h:
+
+/Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/com_example_hello_myapplication_utils_LameMp3Util.h:
--- app/src/main/obj/local/x86/objs/mimp3lame/lame_util.o.d
+++
@@ -1,8 +0,0 @@
-/Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/obj/local/x86/objs/mimp3lame/lame_util.o: \
-  /Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/lame_util.c \
-  /Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/lame_3.99.5_libmp3lame/lame.h \
-  /Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/me_mi_mp3_util_LameUtil.h
-
-/Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/lame_3.99.5_libmp3lame/lame.h:
-
-/Users/hello/Desktop/xzlcorp/AndroidStudioProjects/MyApplication/app/src/main/jni/me_mi_mp3_util_LameUtil.h:
